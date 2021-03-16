ebc1d06f835d31fdbd5e0ddfe0651eb5691034f4
Cataline Baert <snuf_kin@yahoo.com>
2021-03-16 18:20:13 +0100

    Add SVG circle around countdown timer

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -9,20 +9,41 @@ const countdownTimer = () => {
     const stopBtn = document.getElementById('stop');
     const minutesElement = document.querySelector('.minutes');
     const secondsElement = document.querySelector('.seconds');
-    const min = parseInt(minutesElement.innerText);
+    const minutes = parseInt(minutesElement.innerText);
+    const seconds = parseInt(secondsElement.innerText);
+    let TIME_LIMIT = minutes * 60 + seconds;
+    let FULL_DASH_ARRAY = 283;
 
     const setTimer = (minutesElement, secondsElement) => {
       minutesElement.innerText = timer.getTimeValues().toString(['minutes']);
       secondsElement.innerText = timer.getTimeValues().toString(['seconds']);
     };
 
-    const timer = new Timer({countdown: true, startValues: {minutes: min, seconds: 0}, precision: 'seconds'});
+    const timer = new Timer({countdown: true, startValues: {minutes: minutes, seconds: seconds}, precision: 'seconds'});
+
+    const timeToSeconds = (timer) => {
+      return timer.getTimeValues().minutes * 60 + timer.getTimeValues().seconds;
+    }
+    const calculateTimeFraction = (timeLeft) => {
+      const rawTimeFraction = timeLeft / TIME_LIMIT;
+      return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    }
+    const setCircleDasharray = (timeLeft) => {
+      const circleDasharray = `${(
+        calculateTimeFraction(timeLeft, TIME_LIMIT) * FULL_DASH_ARRAY
+      ).toFixed(2)} 283`;
+      document
+        .getElementById("base-timer-path-remaining")
+        .setAttribute("stroke-dasharray", circleDasharray);
+    }
 
     startBtn.addEventListener('click', () => {
       timer.start();
       startBtn.classList.add('hide-control');
       pauseBtn.classList.remove('hide-control');
-      stopBtn.classList.remove('hide-control');
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.add('transition-all');
     });
     pauseBtn.addEventListener('click', () => {
       timer.pause();
@@ -34,17 +55,18 @@ const countdownTimer = () => {
       timer.stop();
       startBtn.classList.remove('hide-control');
       pauseBtn.classList.add('hide-control');
-      stopBtn.classList.add('hide-control');
     });
 
     timer.addEventListener('secondsUpdated', () => {
       setTimer(minutesElement, secondsElement);
+      setCircleDasharray(timeToSeconds(timer));
     });
     timer.addEventListener('started', () => {
       setTimer(minutesElement, secondsElement);
     });
     timer.addEventListener('reset', () => {
       setTimer(minutesElement, secondsElement);
+      setCircleDasharray(timeToSeconds(timer));
     });
   }
 };
